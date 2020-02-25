b298698d2ff26fa7908d131f7a1ef033cc819215
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-25 00:01:39 -0500

    regexed

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/env ruby
-res = ARGV[0].scan(/from:([^\]]+)|to:([^\]]+)|flags:([^\]]+)/)
-fin = []
-for item in res
-  for str in item
-    if str != nil
-      fin += [str]
+begin
+  res = ARGV[0].scan(/from:([^\]]+)|to:([^\]]+)|flags:([^\]]+)/)
+  fin = []
+  for item in res
+    for str in item
+      if str != nil
+        fin += [str]
+      end
     end
   end
+  puts fin.join(',')
+rescue Interrupt => e
+  print_exception(e, true)
+rescue SignalException => e
+    print_exception(e, false)
+rescue Exception => e
+    print_exception(e, false)
 end
-puts fin.join(',')
