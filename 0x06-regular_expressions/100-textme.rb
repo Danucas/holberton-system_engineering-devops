9d07f3d1ee2032fa8052bebe00880ec7426d6d01
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-26 00:57:29 -0500

    dsa

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,15 +1,7 @@
 #!/usr/bin/env ruby
 begin
-  res = ARGV[0].scan(/from:([^\]]+)|to:([^\]]+)|flags:([^\]]+)/)
-  fin = []
-  for item in res
-    for str in item
-      if str != nil
-        fin += [str]
-      end
-    end
-  end
-  puts fin.join(',')
+  res = ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/)
+  puts res.join(',')
 rescue Interrupt => e
   print_exception(e, true)
 rescue SignalException => e
