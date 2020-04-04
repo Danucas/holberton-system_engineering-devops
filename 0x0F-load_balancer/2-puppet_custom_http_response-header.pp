9557d234eda2136d78f70ec70df11f18b76860a0
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-03 22:41:51 -0500

    setup web servers using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -23,7 +23,7 @@ file_line {'header':
     ensure  => 'present',
     path    => '/etc/nginx/sites-available/default',
     after   => 'server_name _;',
-    line    => 'add_header X-Served-By $(hostname);',
+    line    => 'add_header X-Served-By "$(hostname)";',
     require => Package['nginx'],
 }
 
