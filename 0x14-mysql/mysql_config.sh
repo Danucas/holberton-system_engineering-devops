e36fb29a33e77ece31603c78c2eff4b70d9c1988
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-23 12:50:08 -0500

    user configuration

--- 0x14-mysql/mysql_config.sh
+++ 0x14-mysql/mysql_config.sh
@@ -3,4 +3,7 @@ wget http://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server
+echo "Create holberton_user"
+mysql -uroot -p -e "GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn'"
+echo "Show holberton_user permissions"
 mysql -uholberton_user -p -e "SHOW GRANTS FOR 'holberton_user'@'localhost'"
