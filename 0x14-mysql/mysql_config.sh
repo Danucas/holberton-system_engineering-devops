8089380d27aad495d7edf4b802b4af935e3abc98
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-23 12:05:54 -0500

    adding user creation

--- 0x14-mysql/mysql_config.sh
+++ 0x14-mysql/mysql_config.sh
@@ -3,3 +3,4 @@ wget http://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server
+mysql -uroot -p -e "CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';"
