013e02254eaaeb0d4e7f32a44e9aa4accc9fbabc
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-23 12:35:12 -0500

    grant replication client permission

--- 0x14-mysql/mysql_config.sh
+++ 0x14-mysql/mysql_config.sh
@@ -3,4 +3,4 @@ wget http://repo.mysql.com/mysql-apt-config_0.8.9-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.9-1_all.deb
 sudo apt-get update
 sudo apt-get install mysql-server
-mysql -uroot -p -e "CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';"
+mysql -uholberton_user -p -e "SHOW GRANTS FOR 'holberton_user'@'localhost'"
