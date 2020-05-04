f62e8f1d1def3f484518a60a3235486936ca5ea4
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-04 18:24:46 -0500

    implementing csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,9 +5,9 @@ and save response as .csv
 """
 
 
+import csv
 import requests
 import sys
-import csv
 
 
 def main():
@@ -19,25 +19,22 @@ def main():
     us_url += str(sys.argv[1])
     tasks_url = us_url + "/todos"
     todos = requests.get(tasks_url).json()
-    # print(todos)
     user = requests.get(us_url).json()
-    # print("user: ", user.get("id"))
-    if user.get("id"):
-        with open("{}.csv".format(sys.argv[1]), "w") as csv_file:
-            fields = ["USER_ID",
-                      "USER_NAME",
-                      "TASK_COMPLETED_STATUS",
-                      "TASK_TITLE"]
-            writer = csv.DictWriter(csv_file,
-                                    fieldnames=fields,
-                                    quoting=csv.QUOTE_ALL)
-            for task in todos:
-                tmp_dict = {}
-                tmp_dict[fields[0]] = str(sys.argv[1])
-                tmp_dict[fields[1]] = str(user.get("username"))
-                tmp_dict[fields[2]] = str(task.get("completed"))
-                tmp_dict[fields[3]] = str(task.get("title"))
-                writer.writerow(tmp_dict)
+    with open("{}.csv".format(sys.argv[1]), "w") as csv_file:
+        fields = ["USER_ID",
+                  "USER_NAME",
+                  "TASK_COMPLETED_STATUS",
+                  "TASK_TITLE"]
+        writer = csv.DictWriter(csv_file,
+                                fieldnames=fields,
+                                quoting=csv.QUOTE_ALL)
+        for task in todos:
+            tmp_dict = {}
+            tmp_dict[fields[0]] = str(sys.argv[1])
+            tmp_dict[fields[1]] = str(user.get("username"))
+            tmp_dict[fields[2]] = str(task.get("completed"))
+            tmp_dict[fields[3]] = str(task.get("title"))
+            writer.writerow(tmp_dict)
 
 
 if __name__ == '__main__':
