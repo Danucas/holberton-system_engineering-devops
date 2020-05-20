f2d75ace974e2964efbac914ad5631bd09206649
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-20 15:13:01 -0500

    updating service

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
 # Restart Mysql service and copy class-wp-locale.php to class-wp-locale.phpp
 # Not a clean fix
+exec {'start-mysql':
+  command  => '/etc/init.d/mysql restart',
+  provider => shell,
+}
 exec {'fix-wordpress':
-  command  => 'service mysql restart && cp /var/www/html/wp-include/class-wp-locale.php /var/www/html/wp-include/class-wp-locale.phpp',
-  provider => shell
+  command  => 'sudo cp /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  provider => shell,
 }
